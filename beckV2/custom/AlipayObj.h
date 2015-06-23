e2b28111aea4c6e0edb4f527056db2e915a70893
yj <yj@jydeMacBook-Pro.local>
2015-06-24 00:43:00 +0800

    dsfd

---
+++ beckV2/custom/AlipayObj.h
@@ -0,0 +1,13 @@
+//
+//  AlipayObj.h
+//  beckV2
+//
+//  Created by yj on 15/6/24.
+//  Copyright (c) 2015年 yj. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AlipayObj : NSObject
+
+@end
--- beckV2/more/PointShopVC.m
+++ beckV2/more/PointShopVC.m
@@ -9,8 +9,7 @@
 #import "PointShopVC.h"
 #import "BaseViewController.h"
 #import <AlipaySDK/AlipaySDK.h>
-#import "WXApi.h"
-
+#import "WechatObj.h"
 @interface PointShopVC ()
 @property(nonatomic,strong)NSArray *pointAr;
 @property(nonatomic,strong)NSDictionary *selectP;
@@ -133,18 +132,34 @@ static NSString *publicKey=@"MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC4ALzpzgxlXwU
         if (anError==nil) {
             if ([aResponseObject[@"errorcode"] integerValue]==0) {
                 NSString *code=aResponseObject[@"orderSn"];//orderSn值传给支付宝 out_trade_no这个字段  支付宝异步url：/front/notifyUrlAct.htm
-                NSString *paystr=[NSString stringWithFormat:@"partner=\"%@\"&seller_id=\"%@\"&out_trade_no=\"%@\"&subject=\"%@\"&body=\"%@\"&total_fee=\"%@\"&notify_url=\"%@\"&service=\"mobile.securitypay.pay\"&payment_type=\"1\"&_input_charset=\"utf-8\"&it_b_pay=\"30m\"&show_url=\"m.alipay.com\"&sign=\"%@\"&sign_type=RSA",partid,seller,code,@"医百分",[NSString stringWithFormat:@"%@积分",self.selectP[@"point"]],self.selectP[@"money"],@"http://www.zhongxinlan.com/beck/front/notifyUrlAct.htm",privatekey];
-                [[AlipaySDK defaultService] payOrder:paystr fromScheme:@"beck" callback:^(NSDictionary *resultDic) {
+                NSString *product=[NSString stringWithFormat:@"%@积分",self.selectP[@"point"]];
+                NSNumber *money=self.selectP[@"money"];
+                if (index==1) {
+                    NSString *price=[NSString stringWithFormat:@"%zd",[money integerValue]*100];
+                    [[WechatObj sharedSingle] sendPayProduct:product price:price orderNum:aResponseObject[@"orderSn"] Block:^(BaseResp* aResponseObject) {
+                        if ([aResponseObject isKindOfClass:[PayResp class]]) {
+                            PayResp *re=(PayResp*)aResponseObject;
+                            if (re.errCode==0) {
+                                [[OTSAlertView alertWithMessage:@"支付成功" andCompleteBlock:nil] show];
+                            }
+                        }
+                    }];
 
-                    NSNumber *paystatus=resultDic[@"resultStatus"];
-                    if (paystatus.integerValue==9000) {
-                        [[OTSAlertView alertWithMessage:@"支付成功" andCompleteBlock:nil] show];
-                    }else if (paystatus.integerValue==6001){
+                }else{
+                    NSString *paystr=[NSString stringWithFormat:@"partner=\"%@\"&seller_id=\"%@\"&out_trade_no=\"%@\"&subject=\"%@\"&body=\"%@\"&total_fee=\"%@\"&notify_url=\"%@\"&service=\"mobile.securitypay.pay\"&payment_type=\"1\"&_input_charset=\"utf-8\"&it_b_pay=\"30m\"&show_url=\"m.alipay.com\"&sign=\"%@\"&sign_type=RSA",partid,seller,code,@"医百分",product,money,@"http://www.zhongxinlan.com/beck/front/notifyUrlAct.htm",privatekey];
+                    [[AlipaySDK defaultService] payOrder:paystr fromScheme:@"beck" callback:^(NSDictionary *resultDic) {
                         
-                    }else{
-                        [[OTSAlertView alertWithMessage:resultDic[@"memo"] andCompleteBlock:nil] show];
-                    }
-                }];
+                        NSNumber *paystatus=resultDic[@"resultStatus"];
+                        if (paystatus.integerValue==9000) {
+                            [[OTSAlertView alertWithMessage:@"支付成功" andCompleteBlock:nil] show];
+                        }else if (paystatus.integerValue==6001){
+                            
+                        }else{
+                            [[OTSAlertView alertWithMessage:resultDic[@"memo"] andCompleteBlock:nil] show];
+                        }
+                    }];
+
+                }
             }else{
                 [[OTSAlertView alertWithMessage:aResponseObject[@"msg"] andCompleteBlock:nil] show];
             }
